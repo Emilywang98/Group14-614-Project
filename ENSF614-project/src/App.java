4151aa4a2157661c0ebccb35cdb0a050c1b3f354
JintaoWang1 <robertwjt@outlook.com>
2021-12-02 16:20:03 -0700

    Update App.java

--- ENSF614-project/src/App.java
+++ ENSF614-project/src/App.java
@@ -1,10 +1,9 @@
 import View.Login;
-import View.MovieSearchView;
 
 public class App {
 
 	public static void main(String[] args) {
-		MovieSearchView app = new MovieSearchView();
+		Login app = new Login();
 		app.runApp();
 		
 	}
