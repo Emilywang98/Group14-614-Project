ee088e567c77004e75d550f8927eefb1bd980017
Greg Slowski <gslowski@ualberta.ca>
2021-12-02 00:49:04 -0700

    Updating database connection

--- ENSF614-project/src/JDBC/SqlDatabaseConnection.java
+++ ENSF614-project/src/JDBC/SqlDatabaseConnection.java
@@ -8,13 +8,12 @@ import java.sql.Statement;
 
 public class SqlDatabaseConnection {
 
-    private static String query;
-//    private String query;
-
-    public SqlDatabaseConnection(String q){
-        this.query = q;
+    public SqlDatabaseConnection() {
     }
-
+    
+    public void doQuery(String query){
+    	
+    }
 
     // Connect to your database.
     // Replace server name, username, and password with your credentials
@@ -22,7 +21,7 @@ public class SqlDatabaseConnection {
 
         ResultSet resultSet = null;
         String url = "jdbc:mysql://127.0.0.1:3306/?user=root";
-        try (Connection connection = DriverManager.getConnection(url, "root", "Jiajia12!@");
+        try (Connection connection = DriverManager.getConnection(url, "root", "PASSWORD");
              Statement statement = connection.createStatement();) {
 
             // Create and execute a SELECT SQL statement.
