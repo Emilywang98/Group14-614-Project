528ec416dce699d7a6986c567c3bf21093b60e55
Greg Slowski <gslowski@ualberta.ca>
2021-12-02 00:09:14 -0700

    Modifying Login Model and gitignore

--- ENSF614-project/src/JDBC/SqlDatabaseConnection.java
+++ ENSF614-project/src/JDBC/SqlDatabaseConnection.java
@@ -8,42 +8,46 @@ import java.sql.Statement;
 
 public class SqlDatabaseConnection {
 
-    private static String query;
+    private Connection connection;
 //    private String query;
 
-    public SqlDatabaseConnection(String q){
-        this.query = q;
+    public SqlDatabaseConnection(){
+        
+        String connectionUrl = "jdbc:mysql://127.0.0.1:3306/MOVIETHEATRE";
+        String name = "root";
+        String pass = "password";
+        
+        try {
+			connection = DriverManager.getConnection(connectionUrl, name, pass);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public String doQuery(String query) {
+    	
+    	ResultSet resultSet = null;
+    	
+        try (
+                Statement statement = connection.createStatement();) {
+
+               // Create and execute a SELECT SQL statement
+               resultSet = statement.executeQuery(query);
+               
+               String s = "";
+               // Print results from select statement
+               while (resultSet.next()) {
+                   s += resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3);
+               }
+               
+               return s;
+           }
+	       catch (SQLException e) {
+	           e.printStackTrace();
+	       }
+        
+        return "this didnt work";
+
     }
 
-
-    // Connect to your database.
-    // Replace server name, username, and password with your credentials
-    public static void main(String[] args) {
-        String connectionUrl =
-                "jdbc:sqlserver://yourserver.database.windows.net:1433;"
-                        + "database=AdventureWorks;"
-                        + "user=yourusername@yourserver;"
-                        + "password=yourpassword;"
-                        + "encrypt=true;"
-                        + "trustServerCertificate=false;"
-                        + "loginTimeout=30;";
-
-        ResultSet resultSet = null;
-
-        try (Connection connection = DriverManager.getConnection(connectionUrl);
-             Statement statement = connection.createStatement();) {
-
-            // Create and execute a SELECT SQL statement.
-//            String selectSql = "SELECT TOP 10 Title, FirstName, LastName from SalesLT.Customer";
-            resultSet = statement.executeQuery(query);
-
-            // Print results from select statement
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString(2) + " " + resultSet.getString(3));
-            }
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
 }
--- ENSF614-project/src/Model/LoginModel.java
+++ ENSF614-project/src/Model/LoginModel.java
@@ -1,8 +1,32 @@
 package Model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import JDBC.SqlDatabaseConnection;
+
 public class LoginModel {
-
+	
+	SqlDatabaseConnection myConnection;
+	String username;
+	String password;
+	
+	
+	public static void main(String[] args) {
+		LoginModel loginModel = new LoginModel();
+		String s = loginModel.getVerification();
+		System.out.println(s);
+	}
+	
     public LoginModel(){
-
+    	myConnection = new SqlDatabaseConnection();
+    }
+    
+    public String getVerification() {
+    	String s = myConnection.doQuery("SELECT * FROM REGISTEREDUSER");
+    	return s;
     }
 }
