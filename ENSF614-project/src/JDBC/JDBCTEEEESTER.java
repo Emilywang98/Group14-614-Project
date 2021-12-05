d8cad6de07b9df4e1e3944023b1e60e1a488eef5
Greg Slowski <gslowski@ualberta.ca>
2021-12-04 18:14:46 -0700

    changed user test

--- ENSF614-project/src/JDBC/JDBCTEEEESTER.java
+++ ENSF614-project/src/JDBC/JDBCTEEEESTER.java
@@ -7,14 +7,13 @@ public class JDBCTEEEESTER {
 	public static void main(String[] args) {
 		ArrayList<String> testTouple = new ArrayList<String>();
 		String testTableName = "REGISTEREDUSER";
-		testTouple.add("Jacob555");
+		testTouple.add("greg123456789");
 		testTouple.add("12345");
 		testTouple.add("John Smith");
 		testTouple.add(" address goes here");
 		testTouple.add(" email here");
 		testTouple.add(" big number here");
 		testTouple.add("909");
-		testTouple.add("4545");
 		
 
 		try {
