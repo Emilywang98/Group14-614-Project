ff381545a5df8aa7008000ed4ee1adcadbc2e9af
JintaoWang1 <robertwjt@outlook.com>
2021-12-07 17:59:07 -0700

    Create MovieSeatTester.java

---
+++ ENSF614-project/src/MovieSeatTester.java
@@ -0,0 +1,17 @@
+import Controller.MovieController;
+public class MovieSeatTester {
+	
+
+	
+
+		public static void main(String[] args) {
+
+			MovieController movieController = new MovieController();
+
+//			ticketContsetVisible(true);
+
+		}
+
+	
+
+}
