c2146cf187fec93bc55cb582bf182fc7e32cdd3f
Erika Wang <83795961+erikawyt@users.noreply.github.com>
2021-12-07 19:18:18 -0700

    Update TheatreController.java

--- ENSF614-project/src/Controller/TheatreController.java
+++ ENSF614-project/src/Controller/TheatreController.java
@@ -3,17 +3,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import View.TheatreView;
-import Model.TheatreModel;
 
 
 public class TheatreController {
 	
 	TheatreView view;
-	TheatreModel model;
 
 	public TheatreController(){
 	    view = new TheatreView();
-	    model = new TheatreModel();
 	    view.addTheatreListener1(new TheatreListener1());
 	    view.addTheatreListener2(new TheatreListener2());
 	    view.addTheatreListener3(new TheatreListener3());
